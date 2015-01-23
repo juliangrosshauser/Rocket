d49c95aeb3006ff45d7b70cd1ff39942a525b441
Julian Grosshauser <julian@juliangrosshauser.com>
2015-01-23 22:57:37 +0100

    Remove gulp-load-plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,38 @@
-var browserSync = require('browser-sync'),
-    del = require('del'),
-    gulp = require('gulp'),
-    plugins = require('gulp-load-plugins')();
+var browserSync  = require('browser-sync'),
+    del          = require('del'),
+    gulp         = require('gulp'),
+    autoprefixer = require('gulp-autoprefixer'),
+    concat       = require('gulp-concat'),
+    htmlmin      = require('gulp-htmlmin'),
+    imagemin     = require('gulp-imagemin'),
+    minify       = require('gulp-minify-css'),
+    sass         = require('gulp-sass'),
+    size         = require('gulp-size'),
+    uglify       = require('gulp-uglify');
 
 gulp.task('html', function() {
   return gulp.src('html/index.html')
-    .pipe(plugins.size({gzip: false, showFiles: true, title:'un-minified html'}))
-    .pipe(plugins.size({gzip: true, showFiles: true, title:'un-minified gzipped html'}))
-    .pipe(plugins.htmlmin({ collapseWhitespace: true }))
-    .pipe(plugins.size({gzip: false, showFiles: true, title:'minified html'}))
-    .pipe(plugins.size({gzip: true, showFiles: true, title:'minified gzipped html'}))
+    .pipe(size({gzip: false, showFiles: true, title:'un-minified html'}))
+    .pipe(size({gzip: true, showFiles: true, title:'un-minified gzipped html'}))
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(size({gzip: false, showFiles: true, title:'minified html'}))
+    .pipe(size({gzip: true, showFiles: true, title:'minified gzipped html'}))
     .pipe(gulp.dest('dist'))
     .pipe(browserSync.reload({ stream: true }));
 });
 
 gulp.task('sass', function() {
   return gulp.src('sass/style.scss')
-    .pipe(plugins.sass({ style: "expanded" }))
-    .pipe(plugins.size({gzip: false, showFiles: true, title:'un-prefixed css'}))
-    .pipe(plugins.size({gzip: true, showFiles: true, title:'un-prefixed gzipped css'}))
-    .pipe(plugins.autoprefixer('last 2 version', 'safari 5', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-    .pipe(plugins.size({gzip: false, showFiles: true, title:'prefixed css'}))
-    .pipe(plugins.size({gzip: true, showFiles: true, title:'prefixed gzipped css'}))
-    .pipe(plugins.concat('style.min.css'))
-    .pipe(plugins.minifyCss())
-    .pipe(plugins.size({gzip: false, showFiles: true, title:'minified css'}))
-    .pipe(plugins.size({gzip: true, showFiles: true, title:'minified gzipped css'}))
+    .pipe(sass({ style: "expanded" }))
+    .pipe(size({gzip: false, showFiles: true, title:'un-prefixed css'}))
+    .pipe(size({gzip: true, showFiles: true, title:'un-prefixed gzipped css'}))
+    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+    .pipe(size({gzip: false, showFiles: true, title:'prefixed css'}))
+    .pipe(size({gzip: true, showFiles: true, title:'prefixed gzipped css'}))
+    .pipe(concat('style.min.css'))
+    .pipe(minify())
+    .pipe(size({gzip: false, showFiles: true, title:'minified css'}))
+    .pipe(size({gzip: true, showFiles: true, title:'minified gzipped css'}))
     .pipe(gulp.dest('dist/css'))
     .pipe(browserSync.reload({ stream: true }));
 });
@@ -37,19 +44,19 @@ gulp.task('js', function() {
     'node_modules/velocity-animate/velocity.ui.js',
     'js/animations.js'
   ])
-  .pipe(plugins.size({gzip: false, showFiles: true, title:'un-minified js'}))
-  .pipe(plugins.size({gzip: true, showFiles: true, title:'un-minified gzipped js'}))
-  .pipe(plugins.concat('script.min.js'))
-  .pipe(plugins.uglify({ preserveComments: 'some', outSourceMap: false }))
-  .pipe(plugins.size({gzip: false, showFiles: true, title:'minified js'}))
-  .pipe(plugins.size({gzip: true, showFiles: true, title:'minified gzipped js'}))
+  .pipe(size({gzip: false, showFiles: true, title:'un-minified js'}))
+  .pipe(size({gzip: true, showFiles: true, title:'un-minified gzipped js'}))
+  .pipe(concat('script.min.js'))
+  .pipe(uglify({ preserveComments: 'some', outSourceMap: false }))
+  .pipe(size({gzip: false, showFiles: true, title:'minified js'}))
+  .pipe(size({gzip: true, showFiles: true, title:'minified gzipped js'}))
   .pipe(gulp.dest('dist/js'))
   .pipe(browserSync.reload({ stream: true }));
 });
 
 gulp.task('img', function() {
   return gulp.src('img/*')
-    .pipe(plugins.imagemin({ optimizationLevel: 7, progressive: true, interlaced: true }))
+    .pipe(imagemin({ optimizationLevel: 7, progressive: true, interlaced: true }))
     .pipe(gulp.dest('dist/img'))
     .pipe(browserSync.reload({ stream: true }));
 });
